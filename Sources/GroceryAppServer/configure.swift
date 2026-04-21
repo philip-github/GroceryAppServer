75a7036afe211b97b345ea6a70f8d4597fb52b3a
Philip Al-Twal <philipal-twal@Philips-MacBook-Pro.local>
2026-04-21 15:13:00 +0300

    move secrets to environment variables

--- Sources/GroceryAppServer/configure.swift
+++ Sources/GroceryAppServer/configure.swift
@@ -8,10 +8,9 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     
-    try app.databases.use(.postgres(configuration: .init(hostname: "localhost",
-                                                         port: 5432,
-                                                         username: "postgres",
-                                                         database: "grocerydb",
+    try app.databases.use(.postgres(configuration: .init(hostname: Environment.get("DB_HOST_NAME") ?? "",
+                                                         username: Environment.get("DB_USER_NAME") ?? "",
+                                                         database: Environment.get("DB_NAME") ?? "",
                                                          tls: .prefer(.init(configuration: .clientDefault)))), as: .psql)
     
     // register migrations
@@ -24,7 +23,7 @@ public func configure(_ app: Application) async throws {
     try app.register(collection: GroceryController())
     
     // setup JWT hash algorithm
-    await app.jwt.keys.add(hmac: .init(stringLiteral: "my-secret-key"), digestAlgorithm: .sha256)
+    await app.jwt.keys.add(hmac: .init(stringLiteral: Environment.get("JWT_SIGN_KEY") ?? ""), digestAlgorithm: .sha256)
     
     // register routes
     try routes(app)
